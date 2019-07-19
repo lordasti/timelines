439a3548f78f91bac2fe11cae4711229e36e162b
lord_asti <manversil@gmail.com>
2019-07-19 19:51:59 +0200

    Timelines-010 Minor improvements

---
+++ src/test/java/es/manuel/vera/silvestre/util/CombinationTest.java
@@ -0,0 +1,32 @@
+package es.manuel.vera.silvestre.util;
+
+import es.manuel.vera.silvestre.modelo.BonusStats;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+//import static org.mockito.Mockito.*;
+
+public class CombinationTest{
+
+    private static final Logger LOGGER = Logger.getLogger(CombinationTest.class);
+
+    @Test
+    public void shouldGetCombinations(){
+        StopWatch watch = StopWatch.createStarted();
+        List<BonusStats> combinations = Combination.getCombinations();
+        watch.stop();
+
+        assertThat(combinations, hasSize(30));
+
+        LOGGER.info(combinations);
+        LOGGER
+            .info("[" + combinations.size() + "] permutations. Took: " + watch.getTime(TimeUnit.MILLISECONDS) + " ms.");
+    }
+}
---
+++ src/test/java/es/manuel/vera/silvestre/util/PermutationTest.java
@@ -0,0 +1,33 @@
+package es.manuel.vera.silvestre.util;
+
+import es.manuel.vera.silvestre.modelo.Stats;
+import org.apache.commons.lang3.time.StopWatch;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+//import static org.mockito.Mockito.*;
+
+public class PermutationTest{
+
+    private static final Logger LOGGER = Logger.getLogger(PermutationTest.class);
+
+    @Test
+    public void shouldDoPermutation(){
+        List<Stats> stats = Arrays.asList(Stats.values());
+
+        StopWatch watch = StopWatch.createStarted();
+        List<List<Stats>> permutations = Permutation.of(stats);
+        watch.stop();
+
+        assertThat(permutations, hasSize(720));
+
+        LOGGER
+            .info("[" + permutations.size() + "] permutations. Took: " + watch.getTime(TimeUnit.MILLISECONDS) + " ms.");
+    }
+}
