995f3187976ef2f58208316152f420105ad67b26
lord_asti <manversil@gmail.com>
2019-08-10 11:56:45 +0200

    Timelines-013 Added "Candidate is useful?" and "idle Crew"

--- src/main/java/es/manuel/vera/silvestre/Timelines.java
+++ src/main/java/es/manuel/vera/silvestre/Timelines.java
@@ -4,14 +4,9 @@ import es.manuel.vera.silvestre.modelo.*;
 import es.manuel.vera.silvestre.util.AppUtil;
 import es.manuel.vera.silvestre.util.GauntletUtil;
 import es.manuel.vera.silvestre.util.VoyageUtil;
-import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,118 +15,163 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Timelines{
-    private static final Logger LOGGER = Logger.getLogger(App.class);
     private static final String[] CREW_OPTIONS = {"My active crew", "My crew", "Timelines"};
 
     private List<Crew> crewToConsider;
-    private List<String> traits;
 
     private JPanel panel1;
 
-    private JComboBox crewComboBox;
+    private JComboBox<String> crewComboBox;
 
     private JTabbedPane tabbedPane1;
 
-    private JComboBox gauntletPrimaryStatComboBox;
-    private JComboBox gauntletTrait1ComboBox;
-    private JComboBox gauntletTrait2ComboBox;
-    private JComboBox gauntletTrait3ComboBox;
+    private JComboBox<Stats> gauntletPrimaryStatComboBox;
+    private JComboBox<String> gauntletTrait1ComboBox;
+    private JComboBox<String> gauntletTrait2ComboBox;
+    private JComboBox<String> gauntletTrait3ComboBox;
     private JButton calculateGauntlet;
 
     private JSpinner voyageAntimatterSpinner;
-    private JComboBox voyagePrimaryStatComboBox;
-    private JComboBox voyageSecondaryStatComboBox;
-    private JComboBox voyageCMD1ComboBox;
-    private JComboBox voyageDIP1ComboBox;
-    private JComboBox voyageSEC1ComboBox;
-    private JComboBox voyageENG1ComboBox;
-    private JComboBox voyageSCI1ComboBox;
-    private JComboBox voyageMED1ComboBox;
-    private JComboBox voyageCMD2ComboBox;
-    private JComboBox voyageDIP2ComboBox;
-    private JComboBox voyageSEC2ComboBox;
-    private JComboBox voyageENG2ComboBox;
-    private JComboBox voyageSCI2ComboBox;
-    private JComboBox voyageMED2ComboBox;
+    private JComboBox<Stats> voyagePrimaryStatComboBox;
+    private JComboBox<Stats> voyageSecondaryStatComboBox;
+    private JComboBox<String> voyageCMD1ComboBox;
+    private JComboBox<String> voyageDIP1ComboBox;
+    private JComboBox<String> voyageSEC1ComboBox;
+    private JComboBox<String> voyageENG1ComboBox;
+    private JComboBox<String> voyageSCI1ComboBox;
+    private JComboBox<String> voyageMED1ComboBox;
+    private JComboBox<String> voyageCMD2ComboBox;
+    private JComboBox<String> voyageDIP2ComboBox;
+    private JComboBox<String> voyageSEC2ComboBox;
+    private JComboBox<String> voyageENG2ComboBox;
+    private JComboBox<String> voyageSCI2ComboBox;
+    private JComboBox<String> voyageMED2ComboBox;
 
     private JButton calculateVoyage;
 
-    private JList result;
+    private JList<String> result;
     private JButton gauntletBestCrewButton;
     private JButton voyageBestCrewButton;
+    private JComboBox<Crew> candidatesComboBox;
+    private JButton isUsefulButton;
+    private JButton idleCrewButton;
 
     public Timelines(){
-        calculateGauntlet.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                Gauntlet gauntlet =
-                    GauntletUtil
-                        .calculateGauntlet((Stats) gauntletPrimaryStatComboBox.getSelectedItem(),
-                            crewToConsider,
-                            Arrays.asList((String) gauntletTrait1ComboBox.getSelectedItem(),
-                                (String) gauntletTrait2ComboBox.getSelectedItem(),
-                                (String) gauntletTrait3ComboBox.getSelectedItem())
-                        );
-                gauntlet.getSlots().sort(Slot::compareTo);
-                result.setListData(gauntlet.getSlots().toArray());
+        crewComboBox.addActionListener(e -> {
+            String valueSelected = crewComboBox.getItemAt(crewComboBox.getSelectedIndex());
+            switch(valueSelected){
+                case "My active crew":
+                    crewToConsider = AppUtil.getActiveCrew();
+                    break;
+                case "My crew":
+                default:
+                    crewToConsider = AppUtil.getMyCrew();
+                    break;
+                case "Timelines":
+                    crewToConsider = AppUtil.getTimelinesCrew();
+                    break;
             }
         });
 
-        calculateVoyage.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                Voyage voyage =
-                    VoyageUtil.calculateVoyage(new BonusStats((Stats) voyagePrimaryStatComboBox.getSelectedItem(),
-                            (Stats) voyageSecondaryStatComboBox.getSelectedItem()),
+        calculateGauntlet.addActionListener(e -> {
+            Gauntlet gauntlet =
+                GauntletUtil
+                    .calculateGauntlet(
+                        getSelectedValue(gauntletPrimaryStatComboBox),
                         crewToConsider,
-                        Arrays.asList((String) voyageCMD1ComboBox.getSelectedItem(),
-                            (String) voyageCMD2ComboBox.getSelectedItem(),
-                            (String) voyageDIP1ComboBox.getSelectedItem(),
-                            (String) voyageDIP2ComboBox.getSelectedItem(),
-                            (String) voyageSEC1ComboBox.getSelectedItem(),
-                            (String) voyageSEC2ComboBox.getSelectedItem(),
-                            (String) voyageENG1ComboBox.getSelectedItem(),
-                            (String) voyageENG2ComboBox.getSelectedItem(),
-                            (String) voyageSCI1ComboBox.getSelectedItem(),
-                            (String) voyageSCI2ComboBox.getSelectedItem(),
-                            (String) voyageMED1ComboBox.getSelectedItem(),
-                            (String) voyageMED2ComboBox.getSelectedItem()
-                        ), (Integer) voyageAntimatterSpinner.getValue());
-                voyage.getSlots().sort(Slot::compareTo);
-                List<String> printSlots = voyage.getSlots().stream().map(Slot::toString).collect(Collectors.toList());
-                List<String> printData = new ArrayList<>(printSlots);
-                printData.add("Command:" + voyage.getCommand());
-                printData.add("Diplomacy:" + voyage.getDiplomacy());
-                printData.add("Security:" + voyage.getSecurity());
-                printData.add("Engineering:" + voyage.getEngineering());
-                printData.add("Science:" + voyage.getScience());
-                printData.add("Medicine:" + voyage.getMedicine());
-                printData.add("Estimate:" + LocalTime.ofSecondOfDay(voyage.getVoyageEstimate().longValue()));
-
-                result.setListData(printData.toArray());
-            }
+                        Arrays
+                            .asList(getSelectedValue(gauntletTrait1ComboBox),
+                                getSelectedValue(gauntletTrait2ComboBox),
+                                getSelectedValue(gauntletTrait3ComboBox))
+                    );
+            result.setListData(
+                gauntlet.getSlots().stream().sorted(Slot::compareTo).map(Slot::toString).toArray(String[]::new));
         });
 
-        gauntletBestCrewButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                Map<String,Integer> bestCrew = GauntletUtil.calculateBestCrew(crewToConsider);
-                List<String> printData =
-                    bestCrew.entrySet().stream().map(entry -> entry.getKey() + " (" + entry.getValue() + ")")
-                        .collect(Collectors.toList());
-                result.setListData(printData.toArray());
-            }
+        calculateVoyage.addActionListener(e -> {
+            Voyage voyage =
+                VoyageUtil.calculateVoyage(new BonusStats(getSelectedValue(voyagePrimaryStatComboBox),
+                        getSelectedValue(voyageSecondaryStatComboBox)),
+                    crewToConsider,
+                    Arrays.asList(getSelectedValue(voyageCMD1ComboBox),
+                        getSelectedValue(voyageCMD2ComboBox),
+                        getSelectedValue(voyageDIP1ComboBox),
+                        getSelectedValue(voyageDIP2ComboBox),
+                        getSelectedValue(voyageSEC1ComboBox),
+                        getSelectedValue(voyageSEC2ComboBox),
+                        getSelectedValue(voyageENG1ComboBox),
+                        getSelectedValue(voyageENG2ComboBox),
+                        getSelectedValue(voyageSCI1ComboBox),
+                        getSelectedValue(voyageSCI2ComboBox),
+                        getSelectedValue(voyageMED1ComboBox),
+                        getSelectedValue(voyageMED2ComboBox)
+                    ), (Integer) voyageAntimatterSpinner.getValue());
+            List<String> printData =
+                voyage.getSlots().stream().sorted(Slot::compareTo).map(Slot::toString).collect(Collectors.toList());
+            printData.add("Command:" + voyage.getCommand());
+            printData.add("Diplomacy:" + voyage.getDiplomacy());
+            printData.add("Security:" + voyage.getSecurity());
+            printData.add("Engineering:" + voyage.getEngineering());
+            printData.add("Science:" + voyage.getScience());
+            printData.add("Medicine:" + voyage.getMedicine());
+            printData.add("Estimate:" + LocalTime.ofSecondOfDay(voyage.getVoyageEstimate().longValue()));
+
+            result.setListData(printData.toArray(new String[0]));
         });
-        voyageBestCrewButton.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                Map<String,Integer> bestCrew = VoyageUtil.calculateBestCrew(crewToConsider);
-                List<String> printData =
-                    bestCrew.entrySet().stream().map(entry -> entry.getKey() + " (" + entry.getValue() + ")")
-                        .collect(Collectors.toList());
-                result.setListData(printData.toArray());
-            }
+
+        gauntletBestCrewButton.addActionListener(e -> {
+            Map<String,Integer> bestCrew = GauntletUtil.calculateBestCrew(crewToConsider);
+            result.setListData(
+                bestCrew.entrySet().stream().map(entry -> entry.getKey() + " (" + entry.getValue() + ")")
+                    .toArray(String[]::new));
         });
+
+        voyageBestCrewButton.addActionListener(e -> {
+            Map<String,Integer> bestCrew = VoyageUtil.calculateBestCrew(crewToConsider);
+            result.setListData(
+                bestCrew.entrySet().stream().map(entry -> entry.getKey() + " (" + entry.getValue() + ")")
+                    .toArray(String[]::new));
+        });
+
+        idleCrewButton.addActionListener(e -> {
+            Map<String,Integer> bestVoyageCrew = VoyageUtil.calculateBestCrew(crewToConsider);
+            Map<String,Integer> bestGauntletCrew = GauntletUtil.calculateBestCrew(crewToConsider);
+
+            result.setListData(crewToConsider.stream().map(Crew::getName)
+                .filter(crew -> !bestVoyageCrew.containsKey(crew) && !bestGauntletCrew.containsKey(crew))
+                .toArray(String[]::new));
+        });
+
+        isUsefulButton.addActionListener(e -> {
+            Crew candidate = getSelectedValue(candidatesComboBox);
+            List<Crew> myCrew = AppUtil.getMyCrew();
+            List<String> printData = new ArrayList<>();
+
+            myCrew.add(candidate);
+
+            Map<String,Integer> bestVoyageCrew = VoyageUtil.calculateBestCrew(myCrew);
+            if(bestVoyageCrew.containsKey(candidate.getName())){
+                printData
+                    .add("Found useful for voyages. Number of voyages: " + bestVoyageCrew.get(candidate.getName()));
+            }
+
+            Map<String,Integer> bestGauntletCrew = GauntletUtil.calculateBestCrew(myCrew);
+            if(bestGauntletCrew.containsKey(candidate.getName())){
+                printData
+                    .add(
+                        "Found useful for gauntlet. Number of gauntlets: " + bestGauntletCrew.get(candidate.getName()));
+            }
+
+            if(printData.size() == 0){
+                printData.add("Airlock!");
+            }
+
+            result.setListData(printData.toArray(new String[0]));
+        });
+    }
+
+    private <E> E getSelectedValue(JComboBox<E> combo){
+        return combo.getItemAt(combo.getSelectedIndex());
     }
 
     public static void main(String[] args){
@@ -143,33 +183,35 @@ public class Timelines{
         frame.setVisible(true);
     }
 
-    private void createUIComponents() throws IOException, GeneralSecurityException{
+    private void createUIComponents(){
         crewToConsider = AppUtil.getMyCrew();
-        traits = AppUtil.getTraits();
+        String[] traits = AppUtil.getTraits().toArray(new String[0]);
 
-        crewComboBox = new JComboBox(CREW_OPTIONS);
+        crewComboBox = new JComboBox<>(CREW_OPTIONS);
         crewComboBox.setSelectedIndex(1);
 
-        gauntletPrimaryStatComboBox = new JComboBox(Stats.values());
-        gauntletTrait1ComboBox = new JComboBox(traits.toArray());
-        gauntletTrait2ComboBox = new JComboBox(traits.toArray());
-        gauntletTrait3ComboBox = new JComboBox(traits.toArray());
+        gauntletPrimaryStatComboBox = new JComboBox<>(Stats.values());
+        gauntletTrait1ComboBox = new JComboBox<>(traits);
+        gauntletTrait2ComboBox = new JComboBox<>(traits);
+        gauntletTrait3ComboBox = new JComboBox<>(traits);
 
         voyageAntimatterSpinner = new JSpinner();
         voyageAntimatterSpinner.setValue(2500);
-        voyagePrimaryStatComboBox = new JComboBox(Stats.values());
-        voyageSecondaryStatComboBox = new JComboBox(Stats.values());
-        voyageCMD1ComboBox = new JComboBox(traits.toArray());
-        voyageDIP1ComboBox = new JComboBox(traits.toArray());
-        voyageSEC1ComboBox = new JComboBox(traits.toArray());
-        voyageENG1ComboBox = new JComboBox(traits.toArray());
-        voyageSCI1ComboBox = new JComboBox(traits.toArray());
-        voyageMED1ComboBox = new JComboBox(traits.toArray());
-        voyageCMD2ComboBox = new JComboBox(traits.toArray());
-        voyageDIP2ComboBox = new JComboBox(traits.toArray());
-        voyageSEC2ComboBox = new JComboBox(traits.toArray());
-        voyageENG2ComboBox = new JComboBox(traits.toArray());
-        voyageSCI2ComboBox = new JComboBox(traits.toArray());
-        voyageMED2ComboBox = new JComboBox(traits.toArray());
+        voyagePrimaryStatComboBox = new JComboBox<>(Stats.values());
+        voyageSecondaryStatComboBox = new JComboBox<>(Stats.values());
+        voyageCMD1ComboBox = new JComboBox<>(traits);
+        voyageDIP1ComboBox = new JComboBox<>(traits);
+        voyageSEC1ComboBox = new JComboBox<>(traits);
+        voyageENG1ComboBox = new JComboBox<>(traits);
+        voyageSCI1ComboBox = new JComboBox<>(traits);
+        voyageMED1ComboBox = new JComboBox<>(traits);
+        voyageCMD2ComboBox = new JComboBox<>(traits);
+        voyageDIP2ComboBox = new JComboBox<>(traits);
+        voyageSEC2ComboBox = new JComboBox<>(traits);
+        voyageENG2ComboBox = new JComboBox<>(traits);
+        voyageSCI2ComboBox = new JComboBox<>(traits);
+        voyageMED2ComboBox = new JComboBox<>(traits);
+
+        candidatesComboBox = new JComboBox<>(crewToConsider.toArray(new Crew[0]));
     }
 }
--- src/main/java/es/manuel/vera/silvestre/util/AppUtil.java
+++ src/main/java/es/manuel/vera/silvestre/util/AppUtil.java
@@ -5,63 +5,103 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 import es.manuel.vera.silvestre.modelo.Crew;
 import es.manuel.vera.silvestre.util.google.SheetsServiceUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AppUtil{
 
+    private static final Logger LOGGER = Logger.getLogger(AppUtil.class);
     private static final String SPREADSHEET_ID = "1xTaefRpNV_gPHMympcVU-c2uAe8hVIYOjbt3MXBusjc";
 
-    public static List<Crew> getActiveCrew() throws IOException, GeneralSecurityException{
+    public static List<Crew> getActiveCrew(){
         List<List<Object>> rawCrew = getRoster();
         return IntStream.range(0, rawCrew.size()).mapToObj(id -> new Crew(id, rawCrew.get(id))).filter
             (Crew::isActive).collect(Collectors.toList());
     }
 
-    private static List<List<Object>> getRoster() throws IOException, GeneralSecurityException{
-        Sheets sheetsService = SheetsServiceUtil.getSheetsService();
+    private static List<List<Object>> getRoster(){
+        List<List<Object>> rawCrew = new ArrayList<>();
+        try{
+            Sheets sheetsService = SheetsServiceUtil.getSheetsService();
 
-        ValueRange response = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, "'Stats'").execute();
+            ValueRange response = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, "'Stats'").execute();
 
-        List<List<Object>> rawCrew = response.getValues();
+            rawCrew = response.getValues();
 
-        //remove headers
-        rawCrew.remove(0);
-        rawCrew.remove(0);
-        rawCrew.remove(0);
-        rawCrew.remove(0);
+            //remove headers
+            rawCrew.remove(0);
+            rawCrew.remove(0);
+            rawCrew.remove(0);
+            rawCrew.remove(0);
+        }catch(Exception e){
+            LOGGER.error("Exception in getRoster", e);
+        }
 
         return rawCrew;
     }
 
-    public static List<Crew> getMyCrew() throws IOException, GeneralSecurityException{
+    public static List<Crew> getMyCrew(){
         List<List<Object>> rawCrew = getRoster();
         return IntStream.range(0, rawCrew.size()).mapToObj(id -> new Crew(id, rawCrew.get(id))).filter(Crew::hasStars)
             .collect(Collectors.toList());
     }
 
-    public static List<Crew> getTimelinesCrew() throws IOException, GeneralSecurityException{
+    public static List<Crew> getCandidates(){
+        List<List<Object>> rawCrew = getRoster();
+        return IntStream.range(0, rawCrew.size()).mapToObj(id -> new Crew(id, rawCrew.get(id)))
+            .filter(crew -> !crew.hasStars())
+            .collect(Collectors.toList());
+    }
+
+    public static List<Crew> getTimelinesCrew(){
         List<List<Object>> rawCrew = getRoster();
         return IntStream.range(0, rawCrew.size()).mapToObj(id -> new Crew(id, rawCrew.get(id)))
             .collect(Collectors.toList());
     }
 
-    public static List<String> getTraits() throws IOException, GeneralSecurityException{
-        Sheets sheetsService = SheetsServiceUtil.getSheetsService();
+    public static List<String> getTraits(){
+        List<List<Object>> rawData = new ArrayList<>();
 
-        ValueRange response = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, "'Data'").execute();
+        try{
+            Sheets sheetsService = SheetsServiceUtil.getSheetsService();
 
-        List<List<Object>> rawData = response.getValues();
+            ValueRange response = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, "'Data'").execute();
 
-        //remove header
-        rawData.remove(0);
+            rawData = response.getValues();
+
+            //remove header
+            rawData.remove(0);
+        }catch(Exception e){
+            LOGGER.error("Exception in getTraits", e);
+        }
 
         return rawData.stream().map(data -> (String) data.get(7)).filter(StringUtils::isNotEmpty)
             .map(String::toLowerCase)
             .collect(Collectors.toList());
+    }
+
+    public static void serializeToFile(Object o, String fileName){
+        try(ObjectOutputStream oos =
+            new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)))){
+            oos.writeObject(o);
+        }catch(Exception e){
+            LOGGER.error("Exception in serializeToFile", e);
+        }
+    }
+
+    public static <T> T deserializeFromFile(String fileName){
+        try(ObjectInputStream ois =
+            new ObjectInputStream(new BufferedInputStream(new FileInputStream(fileName)))){
+            return (T) ois.readObject();
+        }catch(Exception e){
+            LOGGER.error("Exception in deserializeFromFile", e);
+        }
+
+        return null;
     }
 }
